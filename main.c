fab97a66aa083daeb7bbf5bc3e9ea2f740ecb796
damodred53 <florent.guyard@live.fr>
2025-09-16 11:34:59 +0200

    premier commit

---
+++ main.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <ctype.h>
+
+// I put un boolean value in order to see if we want to cypher or decypher the word in parameters
+// ou au contraire si on veut remettre le code à son état d'origine.
+void cesar(char str[], int shift, bool isHashedYet) {
+    int i = 0;
+
+    while (str[i] != '\0') {
+        str[i] = toupper(str[i]);
+        i++;
+    }
+
+    i = 0;
+
+    // We read the string until the end of the word
+    while (str[i] != '\0') {
+        if (str[i] >= 'A' && str[i] <= 'Z') {
+            // In C language in connected to a number (example : A = 65, Z = 90)
+            // we substrack the value of each caracter by 65 in order to obtain a new defined numeric value
+            int c = str[i] - 'A';
+            // Selon le booléen "isHashedYet" on décale
+            // vers la fin ou le début de l'alphabet.
+            // Depending on the boolean value "isHashedYet" we shift toward or backward through the alaphabet.
+            if (!isHashedYet) {
+                c += shift;
+            } else {
+                c -= shift;
+            }
+            // We need to get back to the beginning of the alphabet is higher than 25
+            c = (c % 26 + 26) % 26;
+            str[i] = c + 'A';
+        }
+        i++;
+    }
+    // Display of the final ciphered / deciphered result
+    printf("%s\n", str);
+}
+
+int main() {
+    char str[] = "HELLO aat";
+    // This call to the function will cypher the string
+    cesar(str, 3, false);
+    // This call to the function will decypher back the same string
+    cesar(str, 3, true);
+    return 0;
+}
